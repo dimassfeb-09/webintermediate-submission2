a934a9d807bad8f0dc1b0722a8f0451966ba7106
Dimas Febriyanto <dimassfeb@gmail.com>
2025-04-17 06:34:04 +0700

    first commit

---
+++ src/utils/push.js
@@ -0,0 +1,55 @@
+const VAPID_PUBLIC_KEY =
+  "BCCs2eonMI-6H2ctvFaWg-UYdDv387Vno_bzUzALpB442r2lCnsHmtrx8biyPi_E-1fSGABK_Qs_GlvPoJJqxbk";
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+
+  const rawData = atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+
+  return outputArray;
+}
+
+export async function subscribeToPush() {
+  try {
+    const registration = await navigator.serviceWorker.ready;
+    const subscription = await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array(
+        "BCCs2eonMI-6H2ctvFaWg-UYdDv387Vno_bzUzALpB442r2lCnsHmtrx8biyPi_E-1fSGABK_Qs_GlvPoJJqxbk"
+      ),
+    });
+
+    const token = localStorage.getItem("token");
+
+    // Ambil hanya data yang dibutuhkan
+    const { endpoint, keys } = subscription.toJSON();
+
+    const response = await fetch(
+      "https://story-api.dicoding.dev/v1/notifications/subscribe",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          endpoint,
+          keys,
+        }),
+      }
+    );
+
+    const result = await response.json();
+    console.log("Push subscription response:", result);
+    alert("Berhasil subscribe push notification!");
+  } catch (error) {
+    console.error("Push subscription failed:", error);
+    alert("Gagal subscribe push notification.");
+  }
+}
