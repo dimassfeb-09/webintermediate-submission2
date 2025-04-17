b36b34199dfbf6b32078c0903012aefd3c2fd0c1
Dimas Febriyanto <dimassfeb@gmail.com>
2025-04-17 07:01:25 +0700

    first commit

--- docs/sw.js
+++ docs/sw.js
@@ -1,24 +1,24 @@
-const CACHE_NAME = 'wardi-cache-v1';
+const CACHE_NAME = "wardi-cache-v1";
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/main.js',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/main.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
